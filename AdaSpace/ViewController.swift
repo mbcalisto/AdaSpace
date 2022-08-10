5ec27712e8ad473b34eb805fb11e01f16037a4d6
Calisto <mateus.bruno.calisto@gmail.com>
2022-08-10 08:37:39 -0300

    Initial Commit

---
+++ AdaSpace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdaSpace
+//
+//  Created by Mateus Calisto on 10/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
