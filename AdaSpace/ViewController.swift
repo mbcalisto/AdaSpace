a046b3cdeb672d3f5743c8dbf85f1d4d2f50b727
Calisto <mateus.bruno.calisto@gmail.com>
2022-08-10 13:57:51 -0300

    innital commit

--- AdaSpace/ViewController.swift
+++ AdaSpace/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    
+    @IBOutlet weak var lbnome: UILabel!
+    
+    var posts: [Posts] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        chamarAPI{(posts) in
+            print("posts: \(posts[0].content)")
+        }
     }
-
-
+    
+    private func chamarAPI(completion: @escaping ([Posts]) -> ()){
+        let url = URL(string: "http://adaspace.local/posts")!
+        
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            print("response: \(String(describing: response))")
+            print("error: \(String(describing: error))")
+            
+            guard let responseData = data else {return}
+            
+            do{
+                let posts = try JSONDecoder().decode([Posts].self, from: responseData)
+                completion(posts)
+                DispatchQueue.main.sync {
+                    self.posts = posts
+                }
+                
+                print("name: \(posts)")
+                
+            }catch let error{
+                print("error: \(error)")
+            }
+        }
+        task.resume()
+    }
+    
 }
+
 
